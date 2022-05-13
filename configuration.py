56caa30e27401e3d8ebea2806c6472db0a222302
agupta54 <anirudhgupta12@gmail.com>
2022-05-13 05:37:36 +0000

    Anirudh | added fairseq infer without vad

--- configuration.py
+++ configuration.py
@@ -1,5 +1,7 @@
-MODEL = "hf"
-USE_VAD = True
+MODEL = "fairseq"
+USE_VAD = False
 HF_MODEL_PATH = "/home/anirudhgupta/asr-inference/models/hindi_hf"
 WAV_PATH = "/home/anirudhgupta/test_hindi.wav"
-USE_LM = True
+USE_LM = False
+FAIRSEQ_MODEL_PATH = '/home/anirudhgupta/asr-inference/models/fseq_hindi'
+FAIRSEQ_CHECKPOINT_NAME = 'hindi_infer.pt'
--- transcribe.py
+++ transcribe.py
@@ -1,11 +1,16 @@
 import configuration
 from hf.infer import Wav2vecHF
+from fseq.infer import load_model_and_generator, get_results
 from utils import wav_to_pcm16
 import soundfile as sf
 import vad.timestamp_generator as tg
 from tqdm import tqdm
 from rich.console import Console
 from rich.traceback import install
+import torch
+from model_item import ModelItem
+from fseq.infer import Wav2VecCtc
+
 
 install()
 console = Console()
@@ -17,13 +22,35 @@ class Transcribe:
         self.vad = vad
         self.wav_path = wav_path
         self.lm = lm
+        self.model_items = {}
+
+        if torch.cuda.is_available():
+            self.cuda = True
+            self.half = True
+        else:
+            self.cuda = False
+            self.half = False
+
         console.log(f"Transcribing audio file {self.wav_path}")
         console.log(f"Using model type [green underline]{self.model_type}[/]")
         console.log(f"Use vad is set to {self.vad}")
         console.log(f"Use LM is set to {self.lm}")
+
         if self.model_type == "hf":
             console.log(f"Model loaded from {configuration.HF_MODEL_PATH}")
             self.model = self.hf_model(configuration.HF_MODEL_PATH)
+        
+        if self.model_type == "fairseq":
+            model_item = ModelItem(configuration.FAIRSEQ_MODEL_PATH, configuration.FAIRSEQ_CHECKPOINT_NAME)
+            console.log(f"Model artifacts loaded from {configuration.FAIRSEQ_MODEL_PATH}")
+            decoder_type = 'kenlm' if self.lm else 'viterbi'
+            console.log(f"Decoder type is {decoder_type}")
+            model, generator = load_model_and_generator(model_item, self.cuda, decoder = decoder_type, half = self.half)
+            model_item.set_generator(generator)
+            model_item.set_model(model)
+            console.log(f":thumbs_up: Model and generator loaded successfully")
+            self.model_items['fseq'] = model_item
+
 
     def hf_model(self, model_path):
         if self.lm:
@@ -52,6 +79,17 @@ class Transcribe:
             else:
 
                 text = self.model.transcribe(self.wav_path, hotwords=hot_words)
+
+        if self.model_type == "fairseq":
+            model_item = self.model_items['fseq']
+            text = get_results(
+                wav_path=self.wav_path,
+                dict_path=model_item.get_dict_file_path(),
+                generator=model_item.get_generator(),
+                use_cuda=self.cuda,
+                model=model_item.get_model(),
+                half=self.half
+            )
 
         console.log(f"Transcription: {text}", style="bold")
 
